4b4f2afcd219a963ccf8f1b707c5582f1455c659
M. Aaraiz Wasim <68289140+Aaraiz07@users.noreply.github.com>
2023-08-27 18:01:30 +0500

    Create a new xcode project

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Aaraiz Wasim on 27/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
